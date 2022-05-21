0b0988a4c18b199f97f9c006b8f2c07836c0b596
RupeshSahu <75201337+RupeshSahu969@users.noreply.github.com>
2022-05-21 18:05:37 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Todo } from './Day2/Todo';
+
+function App() {
+  return (
+    <div className="App">
+    <Todo/>
+    </div>
+  );
+}
+
+export default App;
